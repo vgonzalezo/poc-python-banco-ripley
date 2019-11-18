451ca9dfb42e9f1b8195a786f4f05399936358f4
Victor Manuel Gonzalez Opazo <Victor.Opazo@tsoftglobal.com>
2019-11-18 17:08:27 -0300

    Cambio puerto

--- app.py
+++ app.py
@@ -18,4 +18,4 @@ def captarCliente():
     return json.loads(response, strict=False)
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0', port=8080)
